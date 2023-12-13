5c28076039136bdea486c1e1394f66cd92823aa1
tango <yanghan81@gmail.com>
2023-12-14 01:55:53 +0800

    bat script

---
+++ run-api.bat
@@ -0,0 +1,4 @@
+@echo off
+call .venv\Scripts\activate
+python main.py --output-directory E:\Development\FlaskProject\output\
+pause
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call .venv\Scripts\activate
-python main.py --lowvram --dont-upcast-attention --output-directory E:\Development\FlaskProject\output\
-pause
